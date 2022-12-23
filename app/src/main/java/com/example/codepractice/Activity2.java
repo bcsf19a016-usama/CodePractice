c3420fe1e04f83835e71552b8b29db7d8d63a969
usama riaz <bcsf19a016@pucit.edu.pk>
2022-12-23 18:58:22 +0500

    test the compose email code

--- app/src/main/java/com/example/codepractice/Activity2.java
+++ app/src/main/java/com/example/codepractice/Activity2.java
@@ -2,6 +2,7 @@ package com.example.codepractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Activity2 extends AppCompatActivity {
@@ -11,4 +12,13 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
     }
+
+    public void composeEmail(String address, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, address);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 }
